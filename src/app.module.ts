d74ffca03f872af9c523d3d9328757aeb0059a05
luskas8 <lucas.anjos@franq.com.br>
2025-07-11 14:56:31 -0300

    feat: add logging & env variables from .env
    feat: update version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: ['.env'],
+    }),
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const nodeEnv = configService.get<string>('NODE_ENV');
+        return {
+          pinoHttp: {
+            transport:
+              nodeEnv === 'dev' ? { target: 'pino-pretty' } : undefined,
+            redact: {
+              paths: [
+                'req.headers.authorization',
+                'req.headers.cookie',
+                'req.body.password',
+                'req.body.cpf',
+              ],
+              censor: '********',
+            },
+            enabled: nodeEnv !== 'test',
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
