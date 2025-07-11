89f54f6db19aca8f8528954725f1bee569c187b5
luskas8 <lucas.anjos@franq.com.br>
2025-07-11 16:41:41 -0300

    fix: tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +7,17 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return a redirect message', () => {
+      expect(() => appController.index()).toBeDefined();
+    });
+    it('should return "OK" on health check', () => {
+      expect(appController.healthCheck()).toBe('OK');
     });
   });
 });
